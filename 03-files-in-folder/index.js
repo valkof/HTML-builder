251dc4bec440643d3205bebbfdab93de02e0d527
valkof <vakofanov@g.bstu.by>
2022-05-23 01:18:56 +0300

    refactor: task 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,22 @@
 const { readdir, stat } = require('fs/promises');
-const path = require('path');
+const { join, extname, parse } = require('path');
 
 const nameDir = 'secret-folder';
-const pathDir = path.join(__dirname, nameDir);
+const pathDir = join(__dirname, nameDir);
 
-readdir(pathDir, { withFileTypes: true }).then(dirents => {
-  console.log(`\n ${nameDir} files:`);
-  dirents.forEach(dirent => {
-    if (dirent.isFile()) {
-      const file = path.parse(dirent.name);
-      const pathFile = path.join(pathDir, dirent.name);
-      stat(pathFile).then(stat => {
-        console.log(`${file.name} - ${file.ext.slice(1)} - ${stat.size}b`);
-      });
-    }
+async function getStatsFiles() {
+  const files = await readdir(pathDir, {withFileTypes: true}).then(dirents => {
+    console.log(`\n ${nameDir} files:`);
+    return dirents.filter(dirent => dirent.isFile()); 
   });
-}).catch(err => console.log(err));
+  for (const file of files) {
+    const pathFile = join(pathDir, file.name);
+    await stat(pathFile).then(stat => {
+      console.log(`${parse(pathFile).name} - ${extname(pathFile).slice(1)} - ${Number(stat.size / 1024).toFixed(3)}kb`);
+    });
+  }
+}
+
+(async () => {
+  await getStatsFiles();
+})();
